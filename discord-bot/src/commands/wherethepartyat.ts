e8471c207e9992c7bd36ca1d138aad18de1871ec
Nathaniel Ho <nxho93@gmail.com>
2023-08-21 00:32:55 -0700

    add code to fetch events from sqlite db from discord bot

--- discord-bot/src/commands/wherethepartyat.ts
+++ discord-bot/src/commands/wherethepartyat.ts
@@ -1,6 +1,41 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
-import fs from 'node:fs';
-import { config } from '../config';
+import * as sqlite3 from 'sqlite3';
+import { resolve } from 'node:path';
+
+const eventsDbPath = resolve(__dirname, '../../../pyscraper/events_database.db');
+const db = new sqlite3.Database(eventsDbPath, sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error('Error opening database:', err.message);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+const fetchAllEvents = () => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM events';
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        console.error('Error executing query:', err.message);
+        reject(err.message);
+      } else {
+        // Process the retrieved rows
+        console.log('Query result:', rows);
+        resolve(rows);
+      }
+
+      // Close the database connection
+      db.close((err) => {
+        if (err) {
+          console.error('Error closing database:', err.message);
+          reject(err.message);
+        } else {
+          console.log('Database connection closed');
+        }
+      });
+    });
+  });
+};
 
 export const data = new SlashCommandBuilder()
   .setName('wherethepartyat')
@@ -9,55 +44,8 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: CommandInteraction) {
   await interaction.deferReply();
 
-  // Send a message into the channel where command was triggered from
-  // const inviteText = await nathanielIsSuperSmart();
-  const data = fs.readFileSync('invites/invite.txt');
-  console.log('data', data.toString());
+  const events = await fetchAllEvents();
+  console.log('fetched events', events);
 
-  const ai_headers = {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${config.OPEN_AI_KEY}`,
-  };
-  const model = 'gpt-3.5-turbo';
-  const temperature = 0.5;
-  const maxTokens = 1024;
-
-  const ai_data = {
-    messages: [
-      {
-        role: 'system',
-        content: `Take this text data of events happening nearby and return a random selection of 5 events close to the current time`,
-      },
-      {
-        role: 'user',
-        content: data.toString('utf-8'),
-      },
-    ],
-    temperature: temperature,
-    max_tokens: maxTokens,
-    model: model,
-  };
-
-  try {
-    console.log('before fetch');
-    // Send the GPT-3.5 request using the axios library
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      headers: ai_headers,
-      body: JSON.stringify(ai_data),
-    });
-
-    // Convert the response to a JSON object
-    const responseData = await response.json();
-    console.log('responsedata', responseData);
-
-    // Extract the generated calendar invite from the GPT-3.5 response
-    const events = responseData.choices[0].message.content.trim();
-    console.log('events', events);
-
-    await interaction.editReply(events);
-  } catch (error) {
-    console.log(error);
-    await interaction.editReply('Oops! Something went wrong.');
-  }
+  await interaction.editReply('Hey!');
 }
