b27b2f101384901aba549b868f342eaafad292c7
Roshan Ramankutty <roshan@ncharge.io>
2023-08-22 17:50:49 -0700

    support multiple upload

--- pyscraper/upload-service.py
+++ pyscraper/upload-service.py
@@ -6,22 +6,28 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png'}
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def upload_files():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+        uploaded_files = request.files.getlist('files')
+        filenames = []
+
+        for file in uploaded_files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                filenames.append(filename)
+
+        if filenames:
+            return f"Uploaded images: {', '.join(filenames)}"
+        else:
+            return "No valid files uploaded."
+
     return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
@@ -29,4 +35,4 @@ def uploaded_file(filename):
     return f"Uploaded image: {filename}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
