1ea25e9372bb378a66c244b6cf7800ffae6541e5
Nathaniel Ho <nxho93@gmail.com>
2023-09-02 03:49:40 -0700

    switch to better-sqlite3 because better

--- discord-bot/src/commands/wherethepartyat.ts
+++ discord-bot/src/commands/wherethepartyat.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
-import * as sqlite3 from 'sqlite3';
+import Database from 'better-sqlite3';
 import { resolve } from 'node:path';
 import { config } from '../config';
 
@@ -14,28 +14,13 @@ interface Event {
 
 const eventsDbPath = config.EVENTS_DB_PATH ?? resolve(__dirname, '../../../events_database.db');
 
-const db = new sqlite3.Database(eventsDbPath, sqlite3.OPEN_READONLY, (err) => {
-  if (err) {
-    console.error('Error opening database:', err.message);
-  } else {
-    console.log('Connected to the database');
-  }
+const db = new Database(eventsDbPath, {
+  readonly: true,
 });
 
-const fetchAllEvents = (): Promise<Event[]> => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT * FROM events ORDER BY RANDOM() limit 5';
-    db.all(query, [], (err, rows) => {
-      if (err) {
-        console.error('Error executing query:', err.message);
-        reject(err.message);
-      } else {
-        // Process the retrieved rows
-        console.log('Query result:', rows);
-        resolve(rows as Event[]);
-      }
-    });
-  });
+const fetchAllEvents = (): Event[] => {
+  const stmt = db.prepare('SELECT * FROM events ORDER BY RANDOM() limit 5');
+  return stmt.all() as Event[];
 };
 
 export const data = new SlashCommandBuilder()
@@ -43,32 +28,25 @@ export const data = new SlashCommandBuilder()
   .setDescription('Where the party at?');
 
 export async function execute(interaction: CommandInteraction) {
-  await interaction.deferReply();
-
   try {
-  const events = await fetchAllEvents();
-  const reply = events.map((e) => {
-    const eventName = e.name || "Untitled Event :)";
-    const eventDescription = e.description || "?";
-    const eventAccount = e.account || "?";
-    const eventLocation = e.location || "?";
-    const eventDatetime = e.datetime || "?";
-  
-    return `Event: ${eventName}\n💬${eventDescription}\n🗣️${eventAccount}\n📍${eventLocation}\n📆${eventDatetime}`;
-  }).join('\n\n');
-  
-  await interaction.editReply(reply);
-  } catch (error) {
-    console.error('Error:', error);
-    await interaction.editReply('An error occurred while fetching events.');
-  } finally {
-    // Close the database connection
-    // db.close((err) => {
-    //   if (err) {
-    //     console.error('Error closing database:', err.message);
-    //   } else {
-    //     console.log('Database connection closed');
-    //   }
-    // });
+    await interaction.deferReply();
+
+    const events = fetchAllEvents();
+    const reply = events
+      .map((e) => {
+        const eventName = e.name || 'Untitled Event :)';
+        const eventDescription = e.description || '?';
+        const eventAccount = e.account || '?';
+        const eventLocation = e.location || '?';
+        const eventDatetime = e.datetime || '?';
+
+        return `Event: ${eventName}\n💬${eventDescription}\n🗣️${eventAccount}\n📍${eventLocation}\n📆${eventDatetime}`;
+      })
+      .join('\n\n');
+
+    await interaction.editReply(reply);
+  } catch (e) {
+    console.error('Error occurred for /wherethepartyat', e);
+    await interaction.editReply('Something went super wrong!');
   }
 }
